db5306b4cbfb0c7e43283c9e730c259688a5142d
Bart Roossien <bart.roossien@energygo.nl>
2025-03-26 18:45:04 +0100

    [ZH] Fixed warnings of implicit conversion from unsigned to signed integer (#503)

--- GeneralsMD/Code/Libraries/Source/WWVegas/WWLib/random.cpp
+++ GeneralsMD/Code/Libraries/Source/WWVegas/WWLib/random.cpp
@@ -233,20 +233,25 @@ int Random2Class::operator() (int minval, int maxval)
 **	that the algorithm is not vulnerable to being primed with a weak seed
 **	and thus prevents the algorithm from breaking down as a result.
 */
+
+// SuperHackers patch, @roossienb/Skyaero, 2025-06-03, the compiler gives a warning about an unsigned integer
+// being assigned to a signed integer. Although it looks like the values are all positive as a signed integer, 
+// it's better to be safe than sorry and explicitly cast the values to signed integers,
+// rather than changing the type of the array to an unsigned integer.
 int Random3Class::Mix1[20] = {
-	0x0baa96887, 0x01e17d32c, 0x003bcdc3c, 0x00f33d1b2,
-	0x076a6491d, 0x0c570d85d, 0x0e382b1e3, 0x078db4362,
-	0x07439a9d4, 0x09cea8ac5, 0x089537c5c, 0x02588f55d,
-	0x0415b5e1d, 0x0216e3d95, 0x085c662e7, 0x05e8ab368,
-	0x03ea5cc8c, 0x0d26a0f74, 0x0f3a9222b, 0x048aad7e4
+	static_cast<int>(0x0baa96887), static_cast<int>(0x01e17d32c), static_cast<int>(0x003bcdc3c), static_cast<int>(0x00f33d1b2),
+	static_cast<int>(0x076a6491d), static_cast<int>(0x0c570d85d), static_cast<int>(0x0e382b1e3), static_cast<int>(0x078db4362),
+	static_cast<int>(0x07439a9d4), static_cast<int>(0x09cea8ac5), static_cast<int>(0x089537c5c), static_cast<int>(0x02588f55d),
+	static_cast<int>(0x0415b5e1d), static_cast<int>(0x0216e3d95), static_cast<int>(0x085c662e7), static_cast<int>(0x05e8ab368),
+	static_cast<int>(0x03ea5cc8c), static_cast<int>(0x0d26a0f74), static_cast<int>(0x0f3a9222b), static_cast<int>(0x048aad7e4)
 };
 
 int Random3Class::Mix2[20] = {
-	0x04b0f3b58, 0x0e874f0c3, 0x06955c5a6, 0x055a7ca46,
-	0x04d9a9d86, 0x0fe28a195, 0x0b1ca7865, 0x06b235751,
-	0x09a997a61, 0x0aa6e95c8, 0x0aaa98ee1, 0x05af9154c,
-	0x0fc8e2263, 0x0390f5e8c, 0x058ffd802, 0x0ac0a5eba,
-	0x0ac4874f6, 0x0a9df0913, 0x086be4c74, 0x0ed2c123b
+	static_cast<int>(0x04b0f3b58), static_cast<int>(0x0e874f0c3), static_cast<int>(0x06955c5a6), static_cast<int>(0x055a7ca46),
+	static_cast<int>(0x04d9a9d86), static_cast<int>(0x0fe28a195), static_cast<int>(0x0b1ca7865), static_cast<int>(0x06b235751),
+	static_cast<int>(0x09a997a61), static_cast<int>(0x0aa6e95c8), static_cast<int>(0x0aaa98ee1), static_cast<int>(0x05af9154c),
+	static_cast<int>(0x0fc8e2263), static_cast<int>(0x0390f5e8c), static_cast<int>(0x058ffd802), static_cast<int>(0x0ac0a5eba),
+	static_cast<int>(0x0ac4874f6), static_cast<int>(0x0a9df0913), static_cast<int>(0x086be4c74), static_cast<int>(0x0ed2c123b)
 };
 
 
