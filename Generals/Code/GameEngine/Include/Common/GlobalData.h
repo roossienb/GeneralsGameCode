26074bf918c1a83a71f8a04180669eb54e451f6a
Bart Roossien <bart.roossien@energygo.nl>
2025-03-28 23:08:09 +0100

    [GEN] Fix compiler warnings for uninitialised variables in copy constructors (#499)

--- Generals/Code/GameEngine/Include/Common/GlobalData.h
+++ Generals/Code/GameEngine/Include/Common/GlobalData.h
@@ -511,8 +511,15 @@ private:
 																			override, and return it */
 
 
+	// TheSuperHackers @buildwarningfix Mauller 28/03/2024 incomplete copy constructors causing uninitialised variable warnings in compilation.
+#if defined(_MSC_VER) && _MSC_VER < 1300
 	GlobalData(const GlobalData& that) { DEBUG_CRASH(("unimplemented")); }
 	GlobalData& operator=(const GlobalData& that) { DEBUG_CRASH(("unimplemented")); return *this; }
+#else
+	GlobalData(const GlobalData& that) = delete;
+	GlobalData& operator=(const GlobalData& that) = default;	//Copy constructor is actually used by the new override function.
+#endif
+
 
 };
 
