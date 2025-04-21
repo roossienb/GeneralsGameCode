d9da1391fb10a46bde8d1925c66230d117bf8a25
Bart Roossien <bart.roossien@energygo.nl>
2025-04-21 09:43:50 +0200

    [ZH] Reverting commit 4fe88de9 (#676) causing mismatches in MP and replays with AI (#734).

--- GeneralsMD/Code/GameEngine/Include/GameLogic/GameLogic.h
+++ GeneralsMD/Code/GameEngine/Include/GameLogic/GameLogic.h
@@ -92,9 +92,7 @@ enum
 
 /// Function pointers for use by GameLogic callback functions.
 typedef void (*GameLogicFuncPtr)( Object *obj, void *userData ); 
-typedef std::hash_map<ObjectID, Object *, rts::hash<ObjectID>, rts::equal_to<ObjectID> > ObjectPtrHash;
-typedef ObjectPtrHash::const_iterator ObjectPtrIter;
-
+typedef std::vector<Object*> ObjectPtrVector;
 
 // ------------------------------------------------------------------------------------------------
 /**
@@ -319,7 +317,7 @@ private:
 	WindowLayout *m_background;
 
 	Object* m_objList;																			///< All of the objects in the world.
-	ObjectPtrHash m_objHash;																///< Used for ObjectID lookups
+	ObjectPtrVector m_objVector;
 
 	// this is a vector, but is maintained as a priority queue.
 	// never modify it directly; please use the proper access methods.
@@ -410,11 +408,10 @@ inline Object* GameLogic::findObjectByID( ObjectID id )
 	if( id == INVALID_ID )
 		return NULL;
 
-	ObjectPtrHash::iterator it = m_objHash.find(id);
-	if (it == m_objHash.end())
-		return NULL;
+	if( (size_t)id < m_objVector.size() )
+		return m_objVector[(size_t)id];
 
-	return (*it).second;
+	return NULL;
 }
 
 
